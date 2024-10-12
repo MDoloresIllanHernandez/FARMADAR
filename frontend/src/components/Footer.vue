844f39f06cd88c07484596866b4c9a8415e65a61
Miriam Cegarra <miriam.cegarragarcia@gmail.com>
2024-10-12 12:41:55 +0200

    Creación de la vista del login y de los componentes login y footer

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  
+
+<footer class="rounded-lg shadow m-4 ">
+    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+      <span class="text-sm text-primary-oscuro sm:text-center ">I.E.S. José Planes. Trabajo Final de Grado creado por Miriam, Lola y René © 2024
+    </span>
+    </div>
+</footer>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,8 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import './styles/style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
