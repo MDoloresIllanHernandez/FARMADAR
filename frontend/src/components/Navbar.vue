c351d7284f78f8cfab1eeea57c4e7b0a5fd3dbe4
Miriam Cegarra <miriam.cegarragarcia@gmail.com>
2024-10-26 13:02:07 +0200

    modificacion navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,5 +1,6 @@
 <template>
       <header class="absolute inset-x-0 top-0 z-50 bg-gray-100">
+        <!--Versión de escritorio-->
         <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div class="flex lg:flex-1">
             <a href="#" class="-m-1.5 p-1.5">
@@ -16,8 +17,12 @@
           <div class="hidden lg:flex lg:gap-x-12">
             <a v-for="item in navigation" :key="item.name" :href="item.href" class="rounded-lg p-2 text-xl font-semibold leading-6 text-primary-oscuro hover:bg-gray-50">{{ item.name }}</a>
           </div>
-       
+          <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+            <a href="" class="text-sm font-semibold leading-6 text-gray-900">Cerrar Sesión </a>
+          </div>
         </nav>
+
+        <!--Versión móvil-->
         <Dialog class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
           <div class="fixed inset-0 z-50" />
           <DialogPanel class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-primary-oscuro/10">
@@ -36,7 +41,6 @@
                 <div class="space-y-2 py-6">
                   <a v-for="item in navigation" :key="item.name" :href="item.href" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-primary-oscuro hover:bg-gray-50">{{ item.name }}</a>
                 </div>
-                
               </div>
             </div>
           </DialogPanel>
@@ -50,16 +54,15 @@
   import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
   
   const navigation = [
-    { name: 'Inicio', href: '/inicio' },
     { name: 'Buscador FARMADAR', href: '/buscador-productos' },
     { name: 'Productos', href: '/productos' },
     { name: 'Farmacias', href: '/farmacias' },
     { name: 'Clientes', href: '/clientes' },
-    { name: 'Reservas', href: '/reservas' },
-    { name: 'CerrarSesion', href: '/cerrar-sesion' }
+    { name: 'Reservas', href: '/reservas' }
   ]
-  
+
   const mobileMenuOpen = ref(false)
+  
   </script>
 
 
