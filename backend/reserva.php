0c9ba46ec08758fa8b314bc99cba6145693e1248
Miriam Cegarra <miriam.cegarragarcia@gmail.com>
2024-11-09 10:53:09 +0100

    Creación de reserva en el back

---
+++ backend/reserva.php
@@ -0,0 +1,145 @@
+<?php
+/**
+ *	Script que se usa en los endpoints para trabajar con registros de la tabla RESERVAS
+ *	La clase "reserva.class.php" es la clase del modelo, que representa a un reserva de la tabla
+*/
+require_once 'src/response.php';
+require_once 'src/classes/reserva.class.php';
+require_once 'src/classes/auth.class.php';
+
+// Permitir solicitudes desde cualquier origen (esto es más amplio y no recomendable en producción)
+header("Access-Control-Allow-Origin: *");
+
+// O permitir solicitudes solo desde un origen específico
+// header("Access-Control-Allow-Origin: http://localhost:5173");
+
+// Permitir los métodos que se pueden usar en las solicitudes
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+
+// Permitir los encabezados personalizados
+header("Access-Control-Allow-Headers: Content-Type, Authorization, Api-Key, Farma-User");
+
+
+// Manejar la solicitud preflight (OPTIONS) antes de ejecutar cualquier lógica de verificación
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    // Devolver una respuesta vacía con código 200 para que el navegador permita la solicitud real
+    http_response_code(200);
+    exit();
+}
+
+$auth = new Authentication();
+
+$auth->verify();
+
+$reserva = new reserva();
+
+/**
+ * Se mira el tipo de petición que ha llegado a la API y dependiendo de ello se realiza una u otra accción
+ */
+switch ($_SERVER['REQUEST_METHOD']) {
+	
+	/**
+	 * Si se ha recibido un GET se llama al método get() del modelo y se le pasan los parámetros recibidos por GET en la petición
+	 */
+	case 'GET':
+		$params = $_GET;
+		
+		$reservas = $reserva->get($params);
+
+		$response = array(
+			'result' => 'ok',
+			'reservas' => $reservas
+		);
+
+		Response::result(200, $response);
+
+		break;
+		
+	/**
+	 * Si se recibe un POST, se comprueba si se han recibido parámetros y en caso afirmativo se usa el método insert() del modelo
+	 */
+	case 'POST':
+		$params = json_decode(file_get_contents('php://input'), true);
+
+		if(!isset($params)){
+			$response = array(
+				'result' => 'error',
+				'details' => 'Error en la solicitud'
+			);
+
+			Response::result(400, $response);
+			exit;
+		}
+
+
+		$insert_id = $reserva->insert($params);
+
+		$response = array(
+			'result' => 'ok',
+			'reserva' => $params
+		);
+
+		Response::result(201, $response);
+		break;
+
+	/**
+	 * Cuando es PUT, comprobamos si la petición lleva el id del reserva que hay que actualizar. En caso afirmativo se usa el método update() del modelo.
+	 */
+	case 'PUT':
+		$params = json_decode(file_get_contents('php://input'), true);
+
+		if(!isset($params) || !isset($_GET['id']) || empty($_GET['id'])){
+			$response = array(
+				'result' => 'error',
+				'details' => 'Error en la solicitud'
+			);
+
+			Response::result(400, $response);
+			exit;
+		}
+
+		$reserva->update($_GET['id'], $params);
+
+		$response = array(
+			'result' => 'ok'
+		);
+
+		Response::result(200, $response);	
+		break;
+
+	/**
+	 * Cuando se solicita un DELETE se comprueba que se envíe un id de reserva. En caso afirmativo se utiliza el método delete() del modelo.
+	 */
+	case 'DELETE':
+		if(!isset($_GET['id']) || empty($_GET['id'])){
+			$response = array(
+				'result' => 'error',
+				'details' => 'Error en la solicitud'
+			);
+
+			Response::result(400, $response);
+			exit;
+		}
+
+		$reserva->delete($_GET['id']);
+
+		$response = array(
+			'result' => 'ok'
+		);
+
+		Response::result(200, $response);
+		break;
+
+	/**
+	 * Para cualquier otro tipo de petición se devuelve un mensaje de error 404.
+	 */
+	default:
+		$response = array(
+			'result' => 'error'
+		);
+
+		Response::result(404, $response);
+
+		break;
+}
+?>
---
+++ backend/src/classes/reserva.class.php
@@ -0,0 +1,346 @@
+<?php
+/**
+ * Clase para el modelo que representa a la tabla "reservas".
+ */
+require_once 'src/response.php';
+require_once 'src/database.php';
+
+class Reserva extends Database
+{
+	/**
+	 * Atributo que indica la tabla asociada a la clase del modelo
+	 */
+	private $table = 'reservas';
+
+	/**
+	 * Array con los campos de la tabla que se pueden usar como filtro para recuperar registros
+	 */
+	private $allowedConditions_get = array(
+		'id',
+		'id_prod',
+		'id_farm',
+	);
+
+	/**
+	 * Array con los campos de la tabla que se pueden proporcionar para insertar registros
+	 */
+	private $allowedConditions_insert = array(
+		'id',
+		'id_prod',
+		'id_farm',
+		'fecha',
+		'hora_inicio',
+		'hora_fin',
+		'cantidad',
+		'datos_cliente',
+		'estado'
+		
+	);
+
+	/**
+	 * Método para validar los datos que se mandan para insertar un registro, comprobar campos obligatorios, valores válidos, etc.
+	 */
+	private function validate($data){
+		
+		if(!isset($data['id']) || empty($data['id'])){
+			$response = array(
+				'result' => 'error',
+				'details' => 'El campo id es obligatorio'
+			);
+
+			Response::result(400, $response);
+			exit;
+		}
+		if(!isset($data['id_farm']) || empty($data['id_farm'])){
+			$response = array(
+				'result' => 'error',
+				'details' => 'El campo id_farm es obligatorio'
+			);
+
+			Response::result(400, $response);
+			exit;
+		}
+		if(!isset($data['id_prod']) || empty($data['id_prod'])){
+			$response = array(
+				'result' => 'error',
+				'details' => 'El campo id_prod es obligatorio'
+			);
+
+			Response::result(400, $response);
+			exit;
+		}
+		if(!isset($data['fecha']) || empty($data['fecha'])){
+			$response = array(
+				'result' => 'error',
+				'details' => 'El campo fecha es obligatorio'
+			);
+
+			Response::result(400, $response);
+			exit;
+		}
+
+		if(!isset($data['hora_inicio']) || empty($data['hora_inicio'])){
+			$response = array(
+				'result' => 'error',
+				'details' => 'El campo hora_inicio es obligatorio'
+			);
+
+			Response::result(400, $response);
+			exit;
+		}
+
+		if(!isset($data['hora_fin']) || empty($data['hora_fin'])){
+			$response = array(
+				'result' => 'error',
+				'details' => 'El campo hora_fin es obligatorio'
+			);
+
+			Response::result(400, $response);
+			exit;
+		}
+		
+		//pendiente controlar que la cantidad sea positiva
+		if(!isset($data['cantidad']) || empty($data['cantidad'])){
+			$response = array(
+				'result' => 'error',
+				'details' => 'La cantidad es obligatoria'
+			);
+
+			Response::result(400, $response);
+			exit;
+		}
+
+
+		if(!isset($data['datos_cliente']) || empty($data['datos_cliente'])){
+			$response = array(
+				'result' => 'error',
+				'details' => 'El campo datos_cliente es obligatorio'
+			);
+
+			Response::result(400, $response);
+			exit;
+		}
+
+		if(!isset($data['estado']) || empty($data['estado'])){
+			$response = array(
+				'result' => 'error',
+				'details' => 'El campo estado es obligatorio'
+			);
+
+			Response::result(400, $response);
+			exit;
+		}
+
+		
+		return true;
+	}
+
+	/**
+	 * Método para validar que al modificar un registro, los campos obligatorios están cumplimentados
+	 */
+	private function validateUpdate($data){
+		
+		if(isset($data['id']) && empty($data['id'])){
+			$response = array(
+				'result' => 'error',
+				'details' => 'El campo id no puede estar vacío'
+			);
+
+			Response::result(400, $response);
+			exit;
+		}
+
+		if(isset($data['id_farm']) && empty($data['id_farm'])){
+			$response = array(
+				'result' => 'error',
+				'details' => 'El campo id_farm no puede estar vacío'
+			);
+
+			Response::result(400, $response);
+			exit;
+		}
+
+		if(isset($data['id_prod']) && empty($data['id_prod'])){
+			$response = array(
+				'result' => 'error',
+				'details' => 'El campo id_prod no puede estar vacío'
+			);
+
+			Response::result(400, $response);
+			exit;
+		}
+
+		if(isset($data['fecha']) && empty($data['fecha'])){
+			$response = array(
+				'result' => 'error',
+				'details' => 'El campo fecha no puede estar vacío'
+			);
+
+			Response::result(400, $response);
+			exit;
+		}
+
+		if(isset($data['hora_inicio']) && empty($data['hora_inicio'])){
+			$response = array(
+				'result' => 'error',
+				'details' => 'El campo hora_inicio no puede estar vacío'
+			);
+
+			Response::result(400, $response);
+			exit;
+		}
+
+		if(isset($data['hora_fin']) && empty($data['hora_fin'])){
+			$response = array(
+				'result' => 'error',
+				'details' => 'El campo hora_fin no puede estar vacío'
+			);
+
+			Response::result(400, $response);
+			exit;
+		}
+
+		//pendiente controlar que la cantidad sea positiva
+		if(isset($data['cantidad']) && empty($data['cantidad'])){
+			$response = array(
+				'result' => 'error',
+				'details' => 'La cantidad es obligatoria'
+			);
+
+			Response::result(400, $response);
+			exit;
+		}
+
+		if(isset($data['datos_cliente']) && empty($data['datos_cliente'])){
+			$response = array(
+				'result' => 'error',
+				'details' => 'El campo datos_cliente no puede estar vacío'
+			);
+
+			Response::result(400, $response);
+			exit;
+		}
+
+		if(isset($data['estado']) && empty($data['estado'])){
+			$response = array(
+				'result' => 'error',
+				'details' => 'El campo estado no puede estar vacío'
+			);
+
+			Response::result(400, $response);
+			exit;
+		}
+
+		
+		return true;
+	}
+
+	/**
+	 * Método para recuperar registros, pudiendo indicar algunos filtros 
+	 */
+	public function get($params){
+		foreach ($params as $key => $param) {
+			if(!in_array($key, $this->allowedConditions_get)){
+				unset($params[$key]);
+				$response = array(
+					'result' => 'error',
+					'details' => 'Error en la solicitud'
+				);
+	
+				Response::result(400, $response);
+				exit;
+			}
+		}
+
+		$reservas = parent::getDB($this->table, $params);
+
+		return $reservas;
+	}
+
+	/**
+	 * Método para recuperar registros por un parametro-valor
+	 */
+	public function getByParams($params1, $value1, $params2, $value2 ){
+		
+
+		$reservas = parent::getDB($this->table, array($params1=>$value1, $params2=>$value2));
+
+		return $reservas;
+	}
+
+	/**
+	 * Método para guardar un registro en la base de datos, recibe como parámetro el JSON con los datos a insertar
+	 */
+	public function insert($params)
+	{
+		foreach ($params as $key => $param) {
+			if(!in_array($key, $this->allowedConditions_insert)){
+				unset($params[$key]);
+				$response = array(
+					'result' => 'error',
+					'details' => 'Error en la solicitud'
+				);
+	
+				Response::result(400, $response);
+				exit;
+			}
+		}
+
+		if($this->validate($params)){
+			return parent::insertDB($this->table, $params);
+		}
+	}
+
+	/**
+	 * Método para actualizar un registro en la base de datos, se indica el id del registro que se quiere actualizar
+	 */
+	public function update($id, $params)
+	{
+		foreach ($params as $key => $parm) {
+			if(!in_array($key, $this->allowedConditions_insert)){
+				unset($params[$key]);
+				$response = array(
+					'result' => 'error',
+					'details' => 'Error en la solicitud'
+				);
+	
+				Response::result(400, $response);
+				exit;
+			}
+		}
+
+		if($this->validateUpdate($params)){
+			$affected_rows = parent::updateProductDB($this->table, $id, $params);
+
+			if($affected_rows==0){
+				$response = array(
+					'result' => 'ok',
+					'details' => 'No hubo cambios'
+				);
+
+				Response::result(200, $response);
+				exit;
+			}
+		}
+	}
+
+	/**
+	 * Método para borrar un registro de la base de datos, se indica el id del registro que queremos eliminar
+	 */
+	public function delete($id)
+	{
+		$affected_rows = parent::deleteDB($this->table, $id);
+
+		if($affected_rows==0){
+			$response = array(
+				'result' => 'ok',
+				'details' => 'No hubo cambios'
+			);
+
+			Response::result(200, $response);
+			exit;
+		}
+	}
+}
+
+?>
