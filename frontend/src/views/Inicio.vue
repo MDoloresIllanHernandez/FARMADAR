315499afe20582444fd2fa2d74835af52ecb0c3b
Miriam Cegarra <miriam.cegarragarcia@gmail.com>
2024-12-03 18:21:37 +0100

    bienvenide

--- frontend/src/views/Inicio.vue
+++ frontend/src/views/Inicio.vue
@@ -4,7 +4,7 @@
     <div class="relative isolate px-6 pt-14 lg:px-8">
       <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
         <div class="text-center">
-          <h1 class="text-4xl font-bold sm:text-6xl text-primary-oscuro">Bienvenido/as a</h1>
+          <h1 class="text-4xl font-bold sm:text-6xl text-primary-oscuro">Bienvenido/a a</h1>
           <img class="p-10" src="../assets/farmadar.png" alt="logo">
           <p class="text-xl font-bold sm:text-xl text-primary-oscuro">Tu buscador de medicamentos de confianza</p>
           <div class="mt-10 flex items-center justify-center gap-x-6">
